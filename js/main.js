652528a16d4f38ac347bb0283ab746b8cd18efb7
kahatato <my-github-user-name@users.noreply.github.com>
2020-01-11 20:24:15 +0900

    デザイン変更

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 $(function () {
-    // フェードイン、フェードアウトを繰り返し
-    setInterval(function() {
-      $('#flash-txt').fadeOut('slow', function() {
-        $(this).fadeIn('slow');
-      });
-    }, 200);
+  // フェードイン、フェードアウトを繰り返し
+  setInterval(function () {
+    $('#flash-txt').fadeOut('slow', function () {
+      $(this).fadeIn('slow');
+    });
+  }, 200);
+  // キーボード入力時の処理
+  $('html').keydown(function (e) {
+    switch (e.which) {
+      case 13: // Enter
+        $('.console-box p').removeClass('hidden');
+        $('.console-box p:nth-child(1) span').addClass('hidden');
+        break;
+    }
   });
+  // 要素の表示・非表示
+  $(".click-list").click(function () {
+    $(this).children(".hidden-list").toggle(50);
+  });
+});
