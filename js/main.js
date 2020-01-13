03e5fdab5eac56b2705a3639a29c8fd104774bbf
kahatato <my-github-user-name@users.noreply.github.com>
2020-01-13 20:08:17 +0900

    スクロール実装など

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 $(function () {
+  // 変数宣言
+  // TOPに戻るスクロール
+  var pagetop = $('#top-btn');
+
+  // 上に戻るボタンを非表示する
+  pagetop.hide();
+
+  // TOPに戻るスクロール
+  $('.scroll-spase').scroll(function () {
+    if ($(this).scrollTop() > 1) {
+      // 上に戻るボタンを表示する
+      pagetop.fadeIn();
+    } else {
+      // 上に戻るボタンを非表示する
+      pagetop.fadeOut();
+    }
+  });
+  // 上に戻るボタンをクリックした場合
+  pagetop.click(function () {
+    // トップに戻る
+    $('.scroll-spase').animate({ scrollTop: 0 }, 500);
+    return false;
+  });
   // フェードイン、フェードアウトを繰り返し
   setInterval(function () {
     $('#flash-txt').fadeOut('slow', function () {
@@ -15,10 +38,20 @@ $(function () {
     }
   });
   // 要素の表示・非表示
+  $(".tw-open").click(function () {
+    $("#aside-space").show(0);
+  });
   $(".click-list").click(function () {
     $(this).children(".hidden-list").toggle(50);
   });
-  $(".tw-open").click(function () {
-    $("#aside-space").toggle(50);
+  $(".tw-close").click(function () {
+    $("#aside-space").hide(0);
   });
+  $(".edit-open").click(function () {
+    $("#edit-space").hide(0);
+  });
+  $("#descktop-icon").dblclick(function () {
+    $("#edit-space").show(0);
+  });
+
 });
