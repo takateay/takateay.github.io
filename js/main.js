c30c282c5b4c9f6b068cc73a17c26d85518cd035
kahatato <28295782+kahatato@users.noreply.github.com>
2020-01-20 08:27:43 +0900

    遷移画面作成開始

--- js/main.js
+++ js/main.js
@@ -60,8 +60,17 @@ $(function () {
   $(".edit-open").click(function () {
     $("#edit-space").hide(0);
   });
-  $("#descktop-icon").dblclick(function () {
+  $("#descktop-icon").click(function () {
     $("#edit-space").show(0);
   });
-
+  // input dateに今日日付をセット
+    var today = new Date();
+    today.setDate(today.getDate());
+    var yyyy = today.getFullYear();
+    var mm = ("0"+(today.getMonth()+1)).slice(-2);
+    var dd = ("0"+today.getDate()).slice(-2);
+    document.getElementById("contact-date").value=yyyy+'-'+mm+'-'+dd;  
 });
+function msg001() {
+  alert('ありがとうございます。');
+};
